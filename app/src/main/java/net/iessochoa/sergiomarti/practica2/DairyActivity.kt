59378b6828f80c941040a73832f3f4dcfce3f686
Sergio Martí <sergio.martihee1997@gmail.com>
2023-02-28 21:27:20 +0100

    punto 23

---
+++ app/src/main/java/net/iessochoa/sergiomarti/practica2/DairyActivity.kt
@@ -0,0 +1,11 @@
+package net.iessochoa.sergiomarti.practica2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DairyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dairy)
+    }
+}
